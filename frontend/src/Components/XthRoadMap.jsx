b256ea378985a219a8f59eb43b566da64a4a8426
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-03-03 14:30:28 +0530

    added difficulty level

--- frontend/src/Components/XthRoadMap.jsx
+++ frontend/src/Components/XthRoadMap.jsx
@@ -26,6 +26,11 @@ const XthRoadMap = () => {
   const [test4SocialScience, setTest4SocialScience] = useState(33);
   const [test4Maths, setTest4Maths] = useState(54);
   const [result, setResult] = useState("");
+  const [selectedCourse, setSelectedCourse] = useState("");
+  const [test1Diff, setTest1Diff] = useState("Medium");
+  const [test2Diff, setTest2Diff] = useState("Medium");
+  const [test3Diff, setTest3Diff] = useState("Medium");
+  const [test4Diff, setTest4Diff] = useState("Medium");
   const findRoadmap = async (e) => {
     e.preventDefault();
     setResult("");
@@ -58,23 +63,23 @@ const XthRoadMap = () => {
       alert("marks should be below 100");
     } else {
       let query = `Test Series 1 : English - ${test1English}/100,  Hindi - ${test1Hindi}/100, Mother Tongue  - ${test1MotherLang}/100  ,
-       science - ${test1Science}/100  , social science - ${test1SocialScience}/100,   maths - ${test1Maths}/100,   difficulty level easy
+       science - ${test1Science}/100  , social science - ${test1SocialScience}/100,   maths - ${test1Maths}/100,   difficulty level - ${test1Diff}
 
       Test Series 2 : English ${test2English}/100,  Hindi  -  ${test2Hindi}/100, Mother Tongue -  ${test2MotherLang}/100  ,
-       science - ${test2Science}/100  , social science  -  ${test2SocialScience}/100,   maths  - ${test2Maths}/100,   difficulty level easy
+       science - ${test2Science}/100  , social science  -  ${test2SocialScience}/100,   maths  - ${test2Maths}/100,   difficulty level  - ${test2Diff}
 
       Test Series 3 : English  -  ${test3English}/100,  Hindi  -  ${test3Hindi}/100, Mother Tongue  -  ${test3MotherLang}/100  ,
-       science - ${test3Science}/100  , social science  -  ${test3SocialScience}/100,   maths  -  ${test3Maths}/100,   difficulty level easy
+       science - ${test3Science}/100  , social science  -  ${test3SocialScience}/100,   maths  -  ${test3Maths}/100,   difficulty level  - ${test3Diff}
  
       Test Series 4 : English  -  ${test4English}/100,  Hindi  -  ${test4Hindi}/100,  -  Mother Tongue ${test4MotherLang}/100  ,
-       science - ${test4Science}/100  ,  social science  -  ${test4SocialScience}/100,   maths  - ${test4Maths}/100,   difficulty level easy
+       science - ${test4Science}/100  ,  social science  -  ${test4SocialScience}/100,   maths  - ${test4Maths}/100,   difficulty level  - ${test4Diff}
  
       Passing marks for each subject is 35,
 
       
         These are test series
 
-        Final test diffculty level will be easy
+        Final test diffculty level will be Medium
 
 
         Note for You : if 4 test average for a  subject is less tham 35, it will have 95% chance of failing in final exam
@@ -87,8 +92,10 @@ const XthRoadMap = () => {
         Commerce need strong knowledge on maths
         Arts need strong knowledge in social science and english, hindi and mother
          predict my  final test marks and round up
-        Suggest me top 3 course based on my score `;
+        Suggest me top 3 course based on my score   
+         `;
 
+      console.log(query);
       const res = await fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         headers: {
@@ -108,14 +115,284 @@ const XthRoadMap = () => {
       });
       const data = await res.json();
       //   console.log(data);
-      console.log(data.choices[0].message.content);
+      //   console.log(data.choices[0].message.content);
       setResult(data.choices[0].message.content);
     }
   };
   return (
     <>
-      <form onSubmit={findRoadmap}>
-        <div className="test1">
+      {JSON.stringify(selectedCourse)}
+      {/* <form onSubmit={findRoadmap}>
+        <div className=" test1 tests m-auto  w-4/5 flex  flex-col">
+          <div>
+            <label>English Marks : </label>
+            <input
+              type="number"
+              value={test1English}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest1English(e.target.value)}
+            />
+            <label>Hindi Marks : </label>{" "}
+            <input
+              type="number"
+              value={test1Hindi}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest1Hindi(e.target.value)}
+            />
+            <label>Mother Tongue Marks : </label>{" "}
+            <input
+              type="number"
+              value={test1MotherLang}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest1Motherlang(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Science Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test1Science}
+              placeholder="marks"
+              onChange={(e) => setTest1Science(e.target.value)}
+            />
+            <label>Social Science Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test1SocialScience}
+              placeholder="marks"
+              onChange={(e) => setTest1SocialScience(e.target.value)}
+            />
+            <label>Maths Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test1Maths}
+              placeholder="marks"
+              onChange={(e) => setTest1Maths(e.target.value)}
+            />
+          </div>
+          <label>Difficulty</label>
+          <select placeholder="Difficulty" className="w-fit">
+            <option selected>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </select>
+        </div>
+        <div className="test2  tests  w-4/5 flex  flex-col">
+          <div>
+            <label>English Marks : </label>
+            <input
+              type="number"
+              value={test2English}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest2English(e.target.value)}
+            />
+            <label>Hindi Marks : </label>{" "}
+            <input
+              type="number"
+              value={test2Hindi}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest2Hindi(e.target.value)}
+            />
+            <label>Mother Tongue Marks : </label>{" "}
+            <input
+              type="number"
+              value={test2MotherLang}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest2Motherlang(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Science Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test2Science}
+              placeholder="marks"
+              onChange={(e) => setTest2Science(e.target.value)}
+            />
+            <label>Social Science Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test2SocialScience}
+              placeholder="marks"
+              onChange={(e) => setTest2SocialScience(e.target.value)}
+            />
+            <label>Maths Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test2Maths}
+              placeholder="marks"
+              onChange={(e) => setTest2Maths(e.target.value)}
+            />
+          </div>
+          <select placeholder="Difficulty" className="w-fit">
+            <option selected>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </select>
+        </div>
+        <div className="test3  tests  w-4/5 flex  flex-col ">
+          <div>
+            {" "}
+            <label>English Marks : </label>
+            <input
+              type="number"
+              value={test3English}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest3English(e.target.value)}
+            />
+            <label>Hindi Marks : </label>{" "}
+            <input
+              type="number"
+              value={test3Hindi}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest3Hindi(e.target.value)}
+            />
+            <label>Mother Tongue Marks : </label>{" "}
+            <input
+              type="number"
+              value={test3MotherLang}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest3Motherlang(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Science Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test3Science}
+              placeholder="marks"
+              onChange={(e) => setTest3Science(e.target.value)}
+            />
+            <label>Social Science Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test3SocialScience}
+              placeholder="marks"
+              onChange={(e) => setTest3SocialScience(e.target.value)}
+            />
+            <label>Maths Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test3Maths}
+              placeholder="marks"
+              onChange={(e) => setTest3Maths(e.target.value)}
+            />
+          </div>
+          <select placeholder="Difficulty" className="w-fit">
+            <option selected>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </select>
+        </div>
+        <div className="test4  tests  w-4/5 flex  flex-col">
+          <div>
+            <label>English Marks : </label>
+            <input
+              type="number"
+              value={test4English}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest4English(e.target.value)}
+            />
+            <label>Hindi Marks : </label>{" "}
+            <input
+              type="number"
+              value={test4Hindi}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest4Hindi(e.target.value)}
+            />
+            <label>Mother Tongue Marks : </label>{" "}
+            <input
+              type="number"
+              value={test4MotherLang}
+              min={0}
+              max={100}
+              placeholder="marks"
+              onChange={(e) => setTest4Motherlang(e.target.value)}
+            />
+          </div>
+          <div>
+            {" "}
+            <label>Science Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test4Science}
+              placeholder="marks"
+              onChange={(e) => setTest4Science(e.target.value)}
+            />
+            <label>Social Science Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test4SocialScience}
+              placeholder="marks"
+              onChange={(e) => setTest4SocialScience(e.target.value)}
+            />
+            <label>Maths Marks : </label>
+            <input
+              type="number"
+              min={0}
+              max={100}
+              value={test4Maths}
+              placeholder="marks"
+              onChange={(e) => setTest4Maths(e.target.value)}
+            />
+          </div>
+          <select placeholder="Difficulty" className="w-fit">
+            <option selected>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </select>
+        </div>
+        <button onClick={findRoadmap}>submit</button>
+      </form> */}
+      <form
+        onSubmit={findRoadmap}
+        className="flex p-5 justify-around flex-wrap  "
+      >
+        <div className=" test1 tests w-2/5 flex  flex-col">
           <label>English Marks : </label>
           <input
             type="number"
@@ -143,7 +420,6 @@ const XthRoadMap = () => {
             placeholder="marks"
             onChange={(e) => setTest1Motherlang(e.target.value)}
           />
-          <br />
           <label>Science Marks : </label>
           <input
             type="number"
@@ -171,9 +447,19 @@ const XthRoadMap = () => {
             placeholder="marks"
             onChange={(e) => setTest1Maths(e.target.value)}
           />
-          <br />
+          <label>Difficulty</label>
+          <select
+            placeholder="Difficulty"
+            value={test1Diff}
+            onChange={(e) => setTest1Diff(e.target.value)}
+            className="w-fit"
+          >
+            <option selected>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </select>
         </div>
-        <div className="test2">
+        <div className="test2  tests  w-2/5 flex  flex-col">
           <label>English Marks : </label>
           <input
             type="number"
@@ -201,7 +487,6 @@ const XthRoadMap = () => {
             placeholder="marks"
             onChange={(e) => setTest2Motherlang(e.target.value)}
           />
-          <br />
           <label>Science Marks : </label>
           <input
             type="number"
@@ -229,9 +514,20 @@ const XthRoadMap = () => {
             placeholder="marks"
             onChange={(e) => setTest2Maths(e.target.value)}
           />
-          <br />
+          <select
+            placeholder="Difficulty"
+            value={test2Diff}
+            onChange={(e) => setTest2Diff(e.target.value)}
+            className="w-fit"
+          >
+            <option value={"Easy"} selected>
+              Easy
+            </option>
+            <option value={"Medium"}>Medium</option>
+            <option value={"Hard"}>Hard</option>
+          </select>
         </div>
-        <div className="test3">
+        <div className="test3  tests  w-2/5 flex  flex-col ">
           <label>English Marks : </label>
           <input
             type="number"
@@ -259,7 +555,6 @@ const XthRoadMap = () => {
             placeholder="marks"
             onChange={(e) => setTest3Motherlang(e.target.value)}
           />
-          <br />
           <label>Science Marks : </label>
           <input
             type="number"
@@ -287,9 +582,18 @@ const XthRoadMap = () => {
             placeholder="marks"
             onChange={(e) => setTest3Maths(e.target.value)}
           />
-          <br />
+          <select
+            placeholder="Difficulty"
+            value={test3Diff}
+            onChange={(e) => setTest3Diff(e.target.value)}
+            className="w-fit"
+          >
+            <option selected>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </select>
         </div>
-        <div className="test4">
+        <div className="test4  tests  w-2/5 flex  flex-col">
           <label>English Marks : </label>
           <input
             type="number"
@@ -317,7 +621,6 @@ const XthRoadMap = () => {
             placeholder="marks"
             onChange={(e) => setTest4Motherlang(e.target.value)}
           />
-          <br />
           <label>Science Marks : </label>
           <input
             type="number"
@@ -345,14 +648,35 @@ const XthRoadMap = () => {
             placeholder="marks"
             onChange={(e) => setTest4Maths(e.target.value)}
           />
-          <br />
+          <select
+            placeholder="Difficulty"
+            value={test4Diff}
+            onChange={(e) => setTest4Diff(e.target.value)}
+            className="w-fit"
+          >
+            <option selected>Easy</option>
+            <option>Medium</option>
+            <option>Hard</option>
+          </select>
         </div>
-        <button onClick={findRoadmap}>submit</button>
       </form>
+      <button
+        className="w-1/10 bg-red-500 px-5 py-3 text-3xl  "
+        onClick={findRoadmap}
+      >
+        submit
+      </button>
       <div className="result ">
         {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
         <pre> {result}</pre>
       </div>
+
+      <input
+        type="text"
+        placeholder="Enter Course"
+        value={selectedCourse}
+        onChange={(e) => setSelectedCourse(e.target.value)}
+      />
     </>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
